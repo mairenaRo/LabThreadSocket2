e4a3c3b226eaac64456cf35272f18ba2c45fcafa
mairenaRo <mairenaRo@users.noreply.github.com>
2020-06-11 21:25:43 -0600

    Quitando lineas inservibles

--- src/main/java/Server/Server.java
+++ src/main/java/Server/Server.java
@@ -22,11 +22,8 @@ public class Server{
     
     private ServerSocket server;
     private Socket connection;
-    private DataOutputStream output;
-    private DataInputStream input;
     private final int PORT = 1234;
     private static Cinema cinema;
-    private final int PRICE = 2500;
     
     public void runServer(){
         try {
@@ -46,56 +43,6 @@ public class Server{
         System.out.println("Esperando conexión con el cliente...");
         connection = server.accept();
         System.out.println("Conexión completada\nConexión establecida con "+connection.getInetAddress().getHostAddress());
-    }
-    
-    private void getStreams() throws IOException{
-        output = new DataOutputStream(connection.getOutputStream());
-        output.flush();
-        input = new DataInputStream(connection.getInputStream());
-    }
-    
-    private void processConnection() throws IOException{
-        int totalPrice = 0; 
-        Movie movie;
-        String message = "";
-        String txt = "";
-        message = "Bienvenido al Cine\nDijite la pelicula que desea ver."+cinema.getMoviesString()+"\nDijite S para salir";
-        output.writeUTF(message);
-        message = input.readUTF();
-        if (!message.equals("s")){
-            movie = cinema.getMovie(Integer.parseInt(message));
-            output.writeUTF(movie.getDescription()+"\nDigite la cantidad de hacintos que desea");
-            int tickets = input.readInt();
-            for (int i = 0; i< tickets; i++){
-                int c = 0;
-                int f = 0;
-                boolean bought = true;
-                do {
-                    output.writeUTF("Ingrese por favor la columna y luego la fila.\nL = Libre\nO = Ocupados\nS = Salvados\n" + movie.getSeatsString() + "\nIngrese la COLUMNA:");
-                    c = input.readInt();
-                    output.writeUTF("Ingrese por favor la columna y luego la fila.\nL = Libre\nO = Ocupados\nS = Salvados\n" + movie.getSeatsString() + "\nIngrese la FILA:");
-                    f = input.readInt();
-                    bought = movie.setSeat(c, f);
-                    output.writeBoolean(bought);
-                    if (!bought) {
-                        output.writeUTF("Asiento no seleccionable");
-                    }else{
-                        txt+=""+c+"-"+f+"\n";
-                    }
-                } while (!bought);
-                output.writeUTF("Se han apartado los asientos\n"+txt);
-                totalPrice += PRICE;
-            }
-            output.writeUTF("Asientos seleccionados\n"+movie.getSeatsString());
-            output.writeUTF("Desea confirmar su compra de "+tickets);
-            int a = input.readInt();
-            if(a<2){
-                movie.saveSeatsSelection();
-                output.writeUTF("¡Compra exitosa!\n"+txt+"\nPrecio total a pagar: "+totalPrice);
-            }else{
-                output.writeUTF("Compra cancelada");
-            }
-        }
     }
     
     private void closeConnection(){
